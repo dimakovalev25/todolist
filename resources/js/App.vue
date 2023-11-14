2f405de50e886a10d54a10a1319cd737ec4a5440
dima25 <d.nozdryn@gmail.com>
2023-11-14 18:40:50 +0300

    added vue js and tailwind

---
+++ resources/js/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="px-10 py-12">
+        <div class="text-3xl font-bold underline text-red-400">vue3</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App"
+}
+</script>
+
+<style scoped>
+
+</style>
